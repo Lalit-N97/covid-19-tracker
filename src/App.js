c1c33836c8b5f1d631ea2961899e3fa510261f27
pirate <lalitnegi1997@gmail.com>
2020-09-17 11:07:49 +0530

    fetch data through api call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Chart from "./components/chart/Chart";
+// import Cards from "./components/cards/Cards";
+// import CountryPicker from "./components/country-picker/CountryPicker";
+
+// OR
+// use this method which requires a new index.js file inside components folder
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api/"; // by default in any folder the imported function is searched in index.js file for every folder.
+
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  // this kind of state initialization automatically
+  // constructs the constructer in the backend
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+    this.setState({ data: fetchedData });
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/cards/Cards.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+
+class Cards extends React.Component {
+  render() {
+    return <div>Cards</div>;
+  }
+}
+
+export default Cards;
